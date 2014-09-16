4cb30085e91d6b20b6894506748903a03fe7c495
Enes13 <enese31@hotmail.de>
2014-09-16 11:07:02 +0200

    Class EventSettings added

--- gen/com/reme/R.java
+++ gen/com/reme/R.java
@@ -10,6 +10,9 @@ package com.reme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,19 +27,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090006;
+        public static final int add_button=0x7f090005;
+        public static final int editText1=0x7f090001;
+        public static final int editText2=0x7f090002;
+        public static final int imageView1=0x7f090004;
+        public static final int or_button=0x7f090003;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_homescreen=0x7f030000;
-        public static final int activity_intro=0x7f030001;
+        public static final int activity_eventsettings=0x7f030000;
+        public static final int activity_homescreen=0x7f030001;
+        public static final int activity_intro=0x7f030002;
     }
     public static final class menu {
-        public static final int intro=0x7f070000;
+        public static final int intro=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int and_capitalized=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int event_settings_1=0x7f060004;
+        public static final int or_capitilized=0x7f060003;
     }
     public static final class style {
         /** 
@@ -60,10 +72,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/reme/HomeScreen.java
+++ src/com/reme/HomeScreen.java
@@ -1,8 +1,18 @@
 package com.reme;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeScreen extends Activity {
 
@@ -10,13 +20,49 @@ public class HomeScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_homescreen);
+
+		init_hold_to_add();
+		initInvisibleButton();
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.intro, menu);
 		return true;
 	}
-	
+
+	private void init_hold_to_add() {
+		try {
+			InputStream is = getAssets().open("hold_to_add.png");
+			Drawable d = Drawable.createFromStream(is, null);
+
+			ImageView iv = (ImageView) findViewById(R.id.imageView1);
+			iv.setImageDrawable(d);
+		} catch (IOException e) {
+
+		}
+
+	}
+
+	private void initInvisibleButton() {
+
+		Button b = (Button) findViewById(R.id.add_button);
+		b.setBackgroundColor(Color.TRANSPARENT);
+		b.setOnLongClickListener(new OnLongClickListener() {
+
+			@Override
+			public boolean onLongClick(View v) {
+				Utility.vibrate(80, HomeScreen.this);
+				HomeScreen.this.startEventSettingsActivity();
+				return false;
+			}
+		});
+	}
+
+	public void startEventSettingsActivity() {
+		Intent i = new Intent(getApplicationContext(), EventSettings.class);
+		startActivity(i);
+	}
+
 }
